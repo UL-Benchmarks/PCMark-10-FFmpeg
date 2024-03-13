1d2122e9c3c46a8fc2873e9d5705601a47516e4b
Kimmo Tamminen <kimmo.tamminen@ul.com>
2024-03-13 16:11:18 +0200

    Fixed issue where a session lasting less than a second would report fps of zero

--- fftools/ffmpeg.c
+++ fftools/ffmpeg.c
@@ -540,7 +540,7 @@ static void print_report(int is_last_report, int64_t timer_start, int64_t cur_ti
             float fps;
             uint64_t frame_number = atomic_load(&ost->packets_written);
 
-            fps = t > 1 ? frame_number / t : 0;
+            fps = t > 0 ? frame_number / t : 0;
             av_bprintf(&buf, "frame=%5"PRId64" fps=%3.*f q=%3.1f ",
                      frame_number, fps < 9.95, fps, q);
             av_bprintf(&buf_script, "frame=%"PRId64"\n", frame_number);
